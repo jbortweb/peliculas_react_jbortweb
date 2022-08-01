6767003272efb21b32198c545abecf330253df58
jbortweb <jbortweb@gmail.com>
2022-08-01 16:50:52 +0200

    Reparar títulos de las series

--- src/components/serie/Serie.jsx
+++ src/components/serie/Serie.jsx
@@ -4,7 +4,7 @@ import './Serie.scss';
 const Serie = ({serie}) => {  // Imprime la respuesta de la API
     console.log(serie);
     return <div className='movie'>
-        <h4>{serie.title}</h4>
+        <h4>{serie.name}</h4>
         <img src = {"http://image.tmdb.org/t/p/w185" + serie.poster_path} alt="portada de la pelicúla"/>
         <p>{serie.vote_average}</p>
     </div>
